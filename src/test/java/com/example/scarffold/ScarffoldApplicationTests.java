b3c2553bb2c321f2197bd9696cb3f09c8995ac86
gxdbox <gxd683@126.com>
2022-07-09 11:19:15 +0800

    first

---
+++ src/test/java/com/example/scarffold/ScarffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scarffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScarffoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
